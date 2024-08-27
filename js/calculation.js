fe669ac2eaa6238d8a964999a58a9d4775047aeb
Developer Ujjal <ujjalchandra213@gmail.com>
2024-08-27 17:37:29 +0600

    completed geomatric mathmatical js

--- js/calculation.js
+++ js/calculation.js
@@ -2,10 +2,18 @@ document.getElementById("btn-calculate").addEventListener("click", function(){
     let basic = getValue("base");
     let height = getValue("height");
     let areaResult = 0.5 * basic * height;
+
+    if(isNaN(basic) || isNaN(height)){
+        // document.getElementById("result-count").innerText = "00";
+        return;
+    }
    
     setResult("result-count", areaResult)
-   
+
+    createElement( "title-item1", ".calculate-result-output", areaResult)
+
 })
+
 
 document.getElementById("rectangle-calculate").addEventListener("click", function(){
     let width = getValue("width");
@@ -13,20 +21,81 @@ document.getElementById("rectangle-calculate").addEventListener("click", functio
 
     let areaResult = width * length;
 
+    if(isNaN(width) || isNaN(length)){
+        return;
+    }
+
     setResult("result-count2", areaResult);
+
+    createElement( "title-item2", ".calculate-result-output", areaResult)
 })
+
 
 document.getElementById("parallelogram-calculate").addEventListener("click", function(){
-    let parallelogramBasicElement = document.getElementById("parallelogram-basic");
-    let parallelogramBasicElementText = parallelogramBasicElement.value;
-    let parallelogramBasicValue = parseFloat(parallelogramBasicElementText);
+   let parallelogramBasic = getValue("parallelogram-basic");
+   let parallelogramHeight = getValue("parallelogram-height");
 
-    let parallelogramHeightElement = document.getElementById("parallelogram-height");
-    let parallelogramHeightElementText = parallelogramHeightElement.value;
-    let parallelogramHeightValue = parseFloat(parallelogramHeightElementText);
+   let areaResult = parallelogramBasic * parallelogramHeight;
 
-    let areaTotal = parallelogramBasicValue * parallelogramHeightValue;
-    
-    let resultRlementCount = document.getElementById("result-count3");
-    resultRlementCount.innerText = areaTotal;
+   if(isNaN(parallelogramBasic) || isNaN(parallelogramHeight)){
+    return;
+}
+   setResult("result-count3", areaResult)
+
+   createElement( "title-item3", ".calculate-result-output", areaResult)
 })
+
+
+
+document.getElementById("rhombus-calculate").addEventListener("click", function(){
+    let rhombusBasic = getValue("rhombus-basic");
+    let rhombusHeight = getValue("rhombus-height");
+
+    let areaResult = 0.5 * rhombusBasic * rhombusHeight;
+
+    if(isNaN(rhombusBasic) || isNaN(rhombusHeight)){
+        return;
+    }
+
+    setResult("result-count4", areaResult)
+
+    createElement( "title-item4", ".calculate-result-output", areaResult)
+})
+
+
+
+document.getElementById("pentagon-calculate").addEventListener("click", function(){
+    let pentagonSide = getValue("pentagon-side");
+    let pentagonLength = getValue("pentagon-length");
+
+    let areaResult = 0.5 * pentagonSide * pentagonLength;
+
+    if(isNaN(pentagonSide) || isNaN(pentagonLength)){
+        return;
+    }
+
+    setResult("result-count5", areaResult)
+
+    createElement( "title-item5", ".calculate-result-output", areaResult)
+})
+
+
+
+document.getElementById("ellipse-calculate").addEventListener("click", function(){
+    let ellipseSemiMajor = getValue("ellipse-semi-major");
+    let ellipseSemiMinor = getValue("ellipse-semi-minor");
+
+    let areaResult = Math.PI * ellipseSemiMajor * ellipseSemiMinor;
+    let areaResultTwo = areaResult.toFixed(2);
+
+    if(isNaN(ellipseSemiMajor) || isNaN(ellipseSemiMinor)){
+        return;
+    }
+
+    setResult("result-count6", areaResultTwo);
+
+    createElement( "title-item6", ".calculate-result-output", areaResultTwo)
+})
+
+
+
--- js/utility.js
+++ js/utility.js
@@ -1,4 +1,4 @@
-function getValue(elementId){
+function getValue(elementId) {
     let element = document.getElementById(elementId);
     let valueElementText = element.value;
     let value = parseFloat(valueElementText);
@@ -7,7 +7,32 @@ function getValue(elementId){
 
 
 
-function setResult( elementId, value){
+function setResult(elementId, value) {
     let resultRlement = document.getElementById(elementId);
     resultRlement.innerText = value;
+}
+
+
+let count = 0;
+function createElement( elementId, elementId2, areaResult) {
+    let title = document.getElementById(elementId).innerText;
+    let appendElement = document.querySelector(elementId2)
+
+        let newElement = document.createElement("div");
+        newElement.innerHTML = `
+        
+                        <span>${count += 1}</span>
+                        <span>${title}</span>
+                        <span>${areaResult}</span>
+                        <span>
+                            <button>Conver to M<sup>2</sup></button>
+                        </span>
+        `
+    
+        newElement.classList.add("class", "one-div");
+        appendElement.appendChild(newElement)
+
+
+
+
 }
